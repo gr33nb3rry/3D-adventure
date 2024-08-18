0f1a157fa6b1aa83f76754ed3d9d87fec1c2b42e
Ruslan Bulak <101219597+gr33nb3rry@users.noreply.github.com>
2024-08-18 03:49:18 +0300

    animations + rotations

--- scripts/player_4.gd
+++ scripts/player_4.gd
@@ -1,17 +1,24 @@
 extends CharacterBody3D
 
 @onready var model : Node3D = $Model
-@onready var animation_tree : AnimationTree = $Model/sophia/AnimationTree
+@onready var animation_tree : AnimationTree = $Model/Sophia/AnimationTree
+
+@onready var planet = $/root/World2/Planet
 
 var jump_buffer := 0.0
+const ROTATION_SPEED := 10.0
+const JUMP_VELOCITY := 20.0
 
 func _physics_process(delta: float) -> void:
-	var planet = $/root/World2/Planet
+	%Debug.text = str(Engine.get_frames_per_second())
 	
 	var d = global_position - planet.global_position
 	var gravity_force = (-d.normalized()).normalized()
 	jump_buffer = clamp((jump_buffer - delta * 10.0), 0.0, 50.0)
 	velocity = gravity_force * 10.0 + global_transform.basis.y * jump_buffer
+	
+	if jump_buffer <= 10.0:
+		animation_tree["parameters/conditions/fall"] = true
 	
 	var up_direction = -gravity_force.normalized()
 	var orientation_direction = Quaternion(global_transform.basis.y, up_direction) * global_transform.basis.get_rotation_quaternion()
@@ -28,13 +35,20 @@ func _physics_process(delta: float) -> void:
 	right *= direction.x
 	movement += forward + right
 	if movement != Vector3.ZERO:
-		animation_tree["parameters/conditions/run"] = true
-		model.look_at(global_position + movement, up)
-		velocity += movement.normalized() * 10.0
-	else: animation_tree["parameters/conditions/idle"] = true
-	if Input.is_action_just_pressed("jump"): jump_buffer = 20.0
+		if jump_buffer == 0.0: animation_tree["parameters/conditions/run"] = true
+		$Look/Point.position = Vector3(direction.x, 0, -direction.y)
+		$Model/Sophia.rotation.y = lerp_angle($Model/Sophia.rotation.y, $CamRoot/CamYaw.rotation.y + deg_to_rad(180), ROTATION_SPEED * delta)
+		var model_transform = model.transform.interpolate_with(model.transform.looking_at($Look/Point.position), ROTATION_SPEED * delta)
+		model.transform = model_transform
+
+		velocity += movement.normalized() * 5.0
+	elif jump_buffer == 0.0: animation_tree["parameters/conditions/idle"] = true
+	
 	move_and_slide()
 
 func _input(event):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+	elif Input.is_action_just_pressed("jump") and jump_buffer == 0.0: 
+		jump_buffer = JUMP_VELOCITY
+		animation_tree["parameters/conditions/jump"] = true
